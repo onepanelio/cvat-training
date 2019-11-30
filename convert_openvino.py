e096e79da4c30a6db60acb43ca2c1743350315f8
san999p <57917118+san999p@users.noreply.github.com>
2019-11-29 23:34:21 -0600

    issues resolved with commands

--- convert_openvino.py
+++ convert_openvino.py
@@ -1,20 +1,26 @@
 import os
+from random import randint
+
 # import onepanel
 # from convert_json import converter
 
 
 # TODO: mv to model_optimizer in bash
 # TODO: change script path dynamically
+
+# Check if dataset already exists
+os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/")
 os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
 #generate lable map
 # os.system("python /onepanel/code/convert_json.py path_to_Dataset")
-os.mkdir("onepanel datasets create model_output")
-os.system("mv frozen_inference_graph.bin model_output/")
-os.system("mv frozen_inference_graph.xml model_output/")
+dataset_name = "model_output_{}".format(randint(1000000000, 2000000000))
+os.system("onepanel datasets create {}".format(dataset_name))
+os.system("mv frozen_inference_graph.bin {}/".format(dataset_name))
+os.system("mv frozen_inference_graph.xml {}/".format(dataset_name)
 #if "ssd" in model_name
-os.system("mv /onepanel/code/interp_scripts/ssd_interp.py model_output/")
-os.system("mv label_map.json model_output/")
-os.chdir("model_output")
+os.system("mv /onepanel/code/interp_scripts/ssd_interp.py {}/".format(dataset_name))
+os.system("mv label_map.json {}/".format(dataset_name))
+os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/{}".format(dataset_name))
 os.system("mv ssd_interp.py interp.py")
 os.system('onepanel datasets push -m "update"')
 #
