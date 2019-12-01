23cf6e046af996d49055036e3e7d356152d68c20
san999p <57917118+san999p@users.noreply.github.com>
2019-12-01 00:25:53 -0600

    Add files via upload

--- convert_openvino.py
+++ convert_openvino.py
@@ -10,15 +10,22 @@ from random import randint
 
 # Check if dataset already exists
 os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/")
-os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
+if "ssd" in sys.argv[2]:
+	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
+elif "faster" in sys.argv[2]:
+	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/faster_rcnn_support_api_v1.10.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
+
 #generate lable map
-os.system("python /onepanel/code/convert_json_2.py /onepanel/input/datasets/aleksandr-cluster0-01/cvat-cpu-testing-1-car-88f9cbb714784fe89e9041baa4472bc6d1e8da7f61b9426d8738a77dfcca3ff1/3/label_map.pbtxt")
+os.system("python /onepanel/code/convert_json_2.py {}/label_map.pbtxt".format(sys.argv[1]))
 dataset_name = "modeloutput{}".format(randint(1000000000, 2000000000))
 os.system("onepanel datasets create {}".format(dataset_name))
 os.system("mv frozen_inference_graph.bin {}/".format(dataset_name))
-os.system("mv frozen_inference_graph.xml {}/".format(dataset_name)
-#if "ssd" in model_name
-os.system("mv /onepanel/code/interp_scripts/ssd_interp.py {}/".format(dataset_name))
+os.system("mv frozen_inference_graph.xml {}/".format(dataset_name))
+if "ssd" in sys.argv[2]:
+	os.system("mv /onepanel/code/interp_scripts/ssd_interp.py {}/".format(dataset_name))
+elif "faster" in sys.argv[2]:
+	os.system("mv /onepanel/code/interp_scripts/faster_rcnn.py {}/".format(dataset_name))
+
 os.system("mv label_map.json {}/".format(dataset_name))
 os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/{}".format(dataset_name))
 os.system("mv ssd_interp.py interp.py")
