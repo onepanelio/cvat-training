66eefb62b39976d31c65344d798742826eb86959
san999p <57917118+san999p@users.noreply.github.com>
2019-12-03 00:59:12 -0600

    Add files via upload

--- convert_openvino.py
+++ convert_openvino.py
@@ -9,9 +9,9 @@ import sys
 # TODO: change script path dynamically
 
 # Check if dataset already exists
-os.chdir("/onepanel/code/dldt/model-optimizer/")
+os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/")
 if "ssd" in sys.argv[2]:
-	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
+	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=/onepanel/code/ssd_support_api_v1.14.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
 elif "faster" in sys.argv[2]:
 	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/faster_rcnn_support_api_v1.10.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
 
