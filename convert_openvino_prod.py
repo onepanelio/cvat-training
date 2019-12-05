2af191828f5ac2022cee658e430ae2b75b6781c6
san999p <57917118+san999p@users.noreply.github.com>
2019-12-05 16:05:08 -0600

    Add files via upload

--- convert_openvino_prod.py
+++ convert_openvino_prod.py
@@ -39,8 +39,12 @@ os.system("/onepanel/bin/protoc/bin/protoc object_detection/protos/*.proto --pyt
 os.chdir(params['dataset'])
 os.system('latest=$(find . -name "*.tfrecord*.zip" -print0 | xargs -r -0 ls -1 -t | head -n1) && unzip -o "$latest"')
 if "ssd" in params['model']:
+	if 'epochs' not in params['epochs']:
+		params['epochs'] = 100
 	os.system("python /onepanel/code/create_pipeline_v2.py -in_pipeline /onepanel/input/datasets/aleksandr-cluster0-01/ssd-mobilenet-v2-coco-201/2/pipeline.config -num_classes {} -epochs {} -model /onepanel/input/datasets/aleksandr-cluster0-01/ssd-mobilenet-v2-coco-201/2/model.ckpt -label {}/label_map.pbtxt -train_data {}/car.tfrecord -eval_data {}/car.tfrecord -out_pipeline /onepanel/output/pipeline.config".format(params["num_classes"], params["epochs"], params["dataset"], params["dataset"], params["dataset"]))
 elif "faster-rcnn-resnet101" in params['model']:
+	if 'epochs' not in params['epochs']:
+		params['epochs'] = 100
 	os.system("python /onepanel/code/create_pipeline_v2.py -in_pipeline /onepanel/input/datasets/san999/faster-rcnn-resnet101-coco/1/pipeline.config -num_classes {} -epochs {} -model /onepanel/input/datasets/san999/faster-rcnn-resnet101-coco/1/model.ckpt -label {}/label_map.pbtxt -train_data {}/training.tfrecord -eval_data {}/training.tfrecord -out_pipeline /onepanel/output/pipeline.config".format(params["num_classes"], params["epochs"], params["dataset"], params["dataset"], params["dataset"]))
 
 os.system("python /onepanel/extra_repos/tensorflow_models/research/object_detection/legacy/train.py --train_dir=/onepanel/output/ --pipeline_config_path=/onepanel/output/pipeline.config")
