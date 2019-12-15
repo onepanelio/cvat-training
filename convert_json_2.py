2cf705bb214c79ec780a2c59f24e8b19e2c3a261
san999p <57917118+san999p@users.noreply.github.com>
2019-12-15 12:16:30 -0600

    updated to generate csv file for tf annotation

--- convert_json_2.py
+++ convert_json_2.py
@@ -27,11 +27,44 @@ import sys
 # 	data = open("label_map.json", "r").read()
 # 	print(data)
 
-def converter(path):
+# def converter(path):
+# 	print("reading pbtxt file...", os.path.join(path, "label_map.pbtxt"))
+# 	with open(os.path.join(path, "label_map.pbtxt"),'r') as f:
+# 		txt = f.readlines()
+# 	print("generating label_map.json file...")
+#   f_out = open(os.path.join("label_map.json"),"w")
+#   f_out.write('{ "label_map": { \n')
+# 	data = {}
+# 	for line in txt:
+
+# 		if "id" in line:
+# 			i = str(line.split(":")[1].strip())
+# 			# data["label_map"][i] = ''
+# #           f_out.write('"{}":'.format(i))
+# 			data[i] = None
+# 		if "name"  in line:
+# 			n = line.split(":")[1].strip().strip("'")
+# 			# print(n)
+            
+# 			data[i] = n
+# #           f_out.write('"{}",\n'.format(n))
+# 			# data["label_map"][i] = n.replace('\\"', "\"")
+# 	# print(data)
+# 	d = {"label_map":data}
+# 	with open(os.path.join("/onepanel/output/", "label_map.json"), 'w') as outfile:
+# 		json.dump(d, outfile)
+		
+
+import csv
+
+def converter_withcsv(path):
 	print("reading pbtxt file...", os.path.join(path, "label_map.pbtxt"))
 	with open(os.path.join(path, "label_map.pbtxt"),'r') as f:
 		txt = f.readlines()
 	print("generating label_map.json file...")
+	csv_out = open(os.path.join("/onepanel/output/", "classes.csv"), "w")
+	csv_writer = csv.writer(csv_out)
+	csv_writer.writerow(['labels'])
 #   f_out = open(os.path.join("label_map.json"),"w")
 #   f_out.write('{ "label_map": { \n')
 	data = {}
@@ -45,7 +78,7 @@ def converter(path):
 		if "name"  in line:
 			n = line.split(":")[1].strip().strip("'")
 			# print(n)
-            
+            csv_writer.writerow([n])
 			data[i] = n
 #           f_out.write('"{}",\n'.format(n))
 			# data["label_map"][i] = n.replace('\\"', "\"")
@@ -55,5 +88,6 @@ def converter(path):
 		json.dump(d, outfile)
 
 
+
 if __name__ == "__main__":
-	converter(sys.argv[1])
+	converter_withcsv(sys.argv[1])
