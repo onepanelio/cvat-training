b238f7ebb2f1b50a097c5c7956f5dcc8d450c3cf
san999p <57917118+san999p@users.noreply.github.com>
2020-04-09 13:55:48 -0500

    added comments

--- convert_openvino_final.py
+++ convert_openvino_final.py
@@ -3,6 +3,7 @@ import sys
 from datetime import datetime
 time = datetime.now()
 stamp = time.strftime("%m%d%Y%H%M%S")
+
 # parse parameters
 # sample: epochs=100;num_classes=1
 params = {}
@@ -15,23 +16,22 @@ if 'num_clones' not in params:
 	params['num_clones'] = 1
 	
 
-#TODO: add param for decays
+#TODO: add param for decays and others
 
 
 os.system("pip install test-generator")
 os.system("wget https://github.com/opencv/dldt/archive/2018_R5.zip")
 os.system("unzip 2018_R5.zip")
-#os.system("git clone https://github.com/tensorflow/models.git /onepanel/extra_repos/tensorflow_models")
 os.system("mkdir -p /onepanel/bin/protoc")
 os.system("wget -P /onepanel/bin/protoc https://github.com/protocolbuffers/protobuf/releases/download/v3.10.1/protoc-3.10.1-linux-x86_64.zip")
 os.chdir("/onepanel/bin/protoc/")
 os.system("unzip protoc-3.10.1-linux-x86_64.zip")
 os.chdir("/onepanel/extra_repos/tensorflow_models/research/")
-#os.system("export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim")
 os.system("/onepanel/bin/protoc/bin/protoc object_detection/protos/*.proto --python_out=.")
 os.chdir(params['dataset'])
 os.system('latest=$(find . -name "*.tfrecord*.zip" -print0 | xargs -r -0 ls -1 -t | head -n1) && unzip -o "$latest"')
 
+#fetch approprite base model
 if "ssd-mobilenet-v2-coco" in params['model']:
 	if 'epochs' not in params:
 		params['epochs'] = 15000
@@ -91,16 +91,16 @@ elif "ssd-mobilenet-v1-quantized-coco" in params['model']:
 
 
 
-
+#train and generate the frozen graph
 os.system("python /onepanel/extra_repos/tensorflow_models/research/object_detection/legacy/train.py --train_dir=/onepanel/output/ --pipeline_config_path=/onepanel/output/pipeline.config --num_clones={}".format(params['num_clones']))
 os.system("python /onepanel/extra_repos/tensorflow_models/research/object_detection/export_inference_graph.py --input-type=image_tensor --pipeline_config_path=/onepanel/output/pipeline.config --trained_checkpoint_prefix=/onepanel/output/model.ckpt-{} --output_directory=/onepanel/output".format(params["epochs"]))
 
 
-### python convert_openvino.py /onepanel/input/datasets/aleksandr-cluster0-01/test3-4-car4-f3f6ab924c074a4c89e55957d7dc51ab196e3307e4924f98a3ec7d9f1363a7cd/4 ssd-mobilenet-v2-coco-201
 
 # TODO: mv to model_optimizer in bash
 # TODO: change script path dynamically
 
+#convert to openvino format
 # Check if dataset already exists
 os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/")
 if "ssd" in params['model'] or "ssdlite" in params["model"]:
@@ -109,11 +109,10 @@ elif "frcnn" in params['model'] or "faster-rcnn" in params["model"]:
 	os.system("python mo_tf.py --input_model=/onepanel/output/frozen_inference_graph.pb --tensorflow_use_custom_operations_config=extensions/front/tf/faster_rcnn_support.json --tensorflow_object_detection_api_pipeline_config=/onepanel/output/pipeline.config")
 
 
-
-
 #generate lable map
 os.system("python /onepanel/code/convert_json_2.py {}/".format(params['dataset']))
 dataset_name = "{}-model-output-{}".format(params['model'], stamp)
+#create onepanel dataset for output files
 os.system("onepanel datasets create {}".format(dataset_name))
 os.chdir("/onepanel/code/dldt-2018_R5/model-optimizer/{}".format(dataset_name))
 os.mkdir("tf_annotation_model")
