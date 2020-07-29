552b57ab0f2f1fe736d720ceb8e7b729b66474a6
savan <vsavan7@gmail.com>
2020-07-29 18:37:43 -0500

    take in skip_no as an suer input for video to frames

--- utils/video_to_frames.py
+++ utils/video_to_frames.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import sys
 
-def convert_to_frames(video_path, out_path):
+def convert_to_frames(video_path, out_path, skip_no=7):
     cap = cv2.VideoCapture(video_path)
     if not os.path.exists(out_path):
         os.makedirs(out_path)
@@ -11,12 +11,13 @@ def convert_to_frames(video_path, out_path):
         # print("frame no", frame_no)
         ret, frame = cap.read()
         if ret:
-            cv2.imwrite(os.path.join(out_path, "{}.jpg".format(frame_no)), frame)
+            if frame_no % int(skip_no) == 0:
+                cv2.imwrite(os.path.join(out_path, "{}.jpg".format(frame_no)), frame)
             frame_no += 1
         else:
             break
 
 if __name__ == "__main__":
-    # sameple: python3 video_to_frames.py samepl.mp4 output_folder/
+    # sameple: python3 video_to_frames.py samepl.mp4 output_folder/ 7
     print(sys.argv[1], sys.argv[2])
-    convert_to_frames(sys.argv[1], sys.argv[2])
+    convert_to_frames(sys.argv[1], sys.argv[2], sys.argv[3])
