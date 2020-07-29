e8b40dae7876a1a6fcc08903a999ca00d1673151
savan <vsavan7@gmail.com>
2020-07-29 13:45:40 -0500

    added script for video to frames conversion

---
+++ utils/video_to_frames.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+import sys
+
+def convert_to_frames(video_path, out_path):
+    cap = cv2.VideoCapture(video_path)
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    frame_no = 0
+    while True:
+        # print("frame no", frame_no)
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(os.path.join(out_path, "{}.jpg".format(frame_no)), frame)
+            frame_no += 1
+        else:
+            break
+
+if __name__ == "__main__":
+    # sameple: python3 video_to_frames.py samepl.mp4 output_folder/
+    print(sys.argv[1], sys.argv[2])
+    convert_to_frames(sys.argv[1], sys.argv[2])
