0d2675e1ba5c5bc92e01d4277bca92363ab2481b
savan <vsavan7@gmail.com>
2020-08-07 15:37:02 -0500

    remove manual checkpoint downloading

--- convert_workflow.py
+++ convert_workflow.py
@@ -29,47 +29,47 @@ if not os.path.exists("/mnt/data/models"):
 	os.makedirs("/mnt/data/models")
 
 #check if base model exists, if not then download
-if params['sys-finetune-checkpoint'] == "":
-	print("base model does not exist, downloading...")
+# if params['sys-finetune-checkpoint'] == "":
+# 	print("base model does not exist, downloading...")
 
-	urllib.request.urlretrieve("https://github.com/onepanelio/templates/releases/download/v0.2.0/{}.tar".format(params['model']), "/mnt/data/models/model.tar")
-	model_files = tarfile.open("/mnt/data/models/model.tar")
-	model_files.extractall("/mnt/data/models")
-	model_files.close()
-	model_dir = "/mnt/data/models/"+params['model']
-	files = os.listdir(model_dir)
-	for f in files:
-		shutil.move(model_dir+"/"+f,"/mnt/data/models")
+# 	urllib.request.urlretrieve("https://github.com/onepanelio/templates/releases/download/v0.2.0/{}.tar".format(params['model']), "/mnt/data/models/model.tar")
+# 	model_files = tarfile.open("/mnt/data/models/model.tar")
+# 	model_files.extractall("/mnt/data/models")
+# 	model_files.close()
+# 	model_dir = "/mnt/data/models/"+params['model']
+# 	files = os.listdir(model_dir)
+# 	for f in files:
+# 		shutil.move(model_dir+"/"+f,"/mnt/data/models")
 
-else:
-	with open("/etc/onepanel/artifactRepository") as file:
-		data = yaml.load(file, Loader=yaml.FullLoader)
-	cloud_provider = list(data.keys())[1]
-	bucket_name = data[cloud_provider]['bucket']
-	if cloud_provider == "s3":
-		with open(os.path.join("/etc/onepanel", data[cloud_provider]['accessKeySecret']['key'])) as file:
-            access_key = yaml.load(file, Loader=yaml.FullLoader)
+# else:
+# 	with open("/etc/onepanel/artifactRepository") as file:
+# 		data = yaml.load(file, Loader=yaml.FullLoader)
+# 	cloud_provider = list(data.keys())[1]
+# 	bucket_name = data[cloud_provider]['bucket']
+# 	if cloud_provider == "s3":
+# 		with open(os.path.join("/etc/onepanel", data[cloud_provider]['accessKeySecret']['key'])) as file:
+#             access_key = yaml.load(file, Loader=yaml.FullLoader)
             
-        with open(os.path.join("/etc/onepanel", data[cloud_provider]['secretKeySecret']['key'])) as file:
-            secret_key = yaml.load(file, Loader=yaml.FullLoader)
+#         with open(os.path.join("/etc/onepanel", data[cloud_provider]['secretKeySecret']['key'])) as file:
+#             secret_key = yaml.load(file, Loader=yaml.FullLoader)
 
-        #set env vars
-        os.environ['AWS_ACCESS_KEY_ID'] = access_key
-        os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
-		s3_resource = boto3.resource('s3')
-		bucket = s3_resource.Bucket(bucket_name) 
-		for object in bucket.objects.filter(Prefix = params['sys-finetune-checkpoint']):
-			bucket.download_file(object.key,'/mnt/data/models/'+os.path.basename(object.key))
+#         #set env vars
+#         os.environ['AWS_ACCESS_KEY_ID'] = access_key
+#         os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
+# 		s3_resource = boto3.resource('s3')
+# 		bucket = s3_resource.Bucket(bucket_name) 
+# 		for object in bucket.objects.filter(Prefix = params['sys-finetune-checkpoint']):
+# 			bucket.download_file(object.key,'/mnt/data/models/'+os.path.basename(object.key))
 
-	elif cloud_provider == "gcs":
-		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join("/etc/onepanel", data[cloud_provider]['serviceAccountKeySecret']['key'])
-		storage_client = storage.Client()
-		print("Using GCS...")
-		bucket = storage_client.bucket(bucket_name)
-		blobs = bucket.list_blobs(prefix=params['sys-finetune-checkpoint'])
-		for blob in blobs:
-			print("GCS blob name: ", blob, blob.name)
-			blob.download_to_filename("/mnt/data/models/"+os.path.basename(blob.name))
+# 	elif cloud_provider == "gcs":
+# 		os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join("/etc/onepanel", data[cloud_provider]['serviceAccountKeySecret']['key'])
+# 		storage_client = storage.Client()
+# 		print("Using GCS...")
+# 		bucket = storage_client.bucket(bucket_name)
+# 		blobs = bucket.list_blobs(prefix=params['sys-finetune-checkpoint'])
+# 		for blob in blobs:
+# 			print("GCS blob name: ", blob, blob.name)
+# 			blob.download_to_filename("/mnt/data/models/"+os.path.basename(blob.name))
 
 
 os.system("pip install test-generator")
